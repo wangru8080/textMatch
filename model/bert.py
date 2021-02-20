0f39e0211304cf08b0880e677a46ffb241a97bc4
wangru <wangru8080@163.com>
2021-02-21 01:27:59 +0800

    Update bert.py

--- model/bert.py
+++ model/bert.py
@@ -17,7 +17,7 @@ class BertOrigin(nn.Module):
         super(BertOrigin, self).__init__()
         self.args = args
 
-        self.bert = BertModel.from_pretrained(args.bert_path) # bert-base-chinese
+        self.bert = BertModel.from_pretrained(args.pretrained_model_path) # bert-base-chinese
         self.fc = nn.Sequential(OrderedDict([
             ('fc1', nn.Linear(768, 2)),
             ('dropout1', nn.Dropout(0.5)),
