2e624b8b72db9887248f52c28d203d9ef417140e
wangru <wangru8080@163.com>
2021-02-20 16:05:00 +0800

    Update bert.py

--- model/bert.py
+++ model/bert.py
@@ -10,8 +10,7 @@ import torch.nn as nn
 from torch.nn import functional as F 
 import numpy as np 
 from collections import OrderedDict
-from pytorch_pretrained_bert import BertModel
-torch.manual_seed(2021)
+from transformers import BertModel
 
 class BertOrigin(nn.Module):
     def __init__(self, args):
@@ -30,8 +29,17 @@ class BertOrigin(nn.Module):
         # output_all_encoded_layers=False shape:[batch_size, seq_len, bert_dim=768]
         # output_all_encoded_layers=True list_len=12, all_encoder_layers[0] shape:[batch_size, seq_len, bert_dim=768]
         # pooled_output shape [batch_size, bert_dim=768]
-        all_encoder_layers, pooled_output = self.bert(input_ids, segment_ids, input_mask)
-        print(pooled_output)
+        # from pytorch_pretrained_bert import BertModel
+        # all_encoder_layers, pooled_output = self.bert(input_ids, segment_ids, input_mask)
+        
+        # last_encoder_layers shape:[batch_size, seq_len, bert_dim=768] output all word embedding
+        # all_encoder_layers output_hidden_states=True list_len=12, all_encoder_layers[0] shape:[batch_size, seq_len, bert_dim=768]
+        # pooled_output shape [batch_size, bert_dim=768]
+        bert_out = self.bert(input_ids=input_ids, attention_mask=input_mask, token_type_ids=segment_ids, output_hidden_states=True)
+        last_encoder_layers = bert_out[0]
+        pooled_output = bert_out[1]
+        embedding_output = bert_out[2][0]
+        all_encoder_layers = bert_out[2][1:]
 
         out = self.fc(pooled_output)
         return out
