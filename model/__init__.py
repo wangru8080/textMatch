6b0ab7f87a02341b375b8443c064c05396b55cc2
wangru <wangru8080@163.com>
2021-03-01 21:31:50 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,14 @@
-
+from .bert import (
+    BertOrigin, 
+    BertCNN, 
+    SiameseBert, 
+    SiameseBertAtt, 
+    BertConcatALLCLSToken
+)
+from .albert import (
+    AlbertOrigin, 
+    AlbertOriginLarge, 
+    AlbertOriginXLarge
+)
+from .cnn import SiameseCharCNN
+from .esim import ESIM
