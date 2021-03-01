e01c2987917e2a92d6bc49cfdbc1b491d5d866ff
wangru <wangru8080@163.com>
2021-03-01 21:30:57 +0800

    Update and rename SiameseCharCNN.py to cnn.py

--- model/cnn.py
+++ model/cnn.py
@@ -10,14 +10,23 @@ import torch.nn as nn
 from torch.nn import functional as F 
 import numpy as np 
 from collections import OrderedDict
-from script.MyLayers import EuclideanLayer
+class EuclideanLayer(nn.Module):
+    def __init__(self, dim=1, eps=1e-8, keepdim=False):
+        super(EuclideanLayer, self).__init__()
+
+        self.dim = dim
+        self.eps = eps 
+        self.keepdim = keepdim
+
+    def forward(self, x1, x2):
+        return torch.sqrt(torch.sum(torch.square(x1 - x2), axis=self.dim, keepdim=self.keepdim) + self.eps)
 
 class SiameseCharCNN(nn.Module):
     def __init__(self, args):
         super(SiameseCharCNN, self).__init__()
         self.args = args
 
-        self.embedding = nn.Embedding(self.args.vocab_size, self.args.embedding_size)
+        self.embedding = nn.Embedding(self.args.vocab_size, self.args.embedding_size) # [batch, seq_len, embedding_size]
         self.cnn_list = nn.ModuleList([
             nn.Sequential(OrderedDict([
                 ('conv1d', nn.Conv1d(self.args.embedding_size, self.args.filters, kernel_size)),
@@ -26,7 +35,7 @@ class SiameseCharCNN(nn.Module):
             ]))
             for kernel_size in self.args.kernel_size_list   
         ])
-        self.gap = nn.AdaptiveAvgPool1d(1) 
+        
         self.cos_sim_layer = nn.CosineSimilarity(dim=-1, eps=1e-5)
         self.euclidean_layer = EuclideanLayer(dim=-1, eps=1e-5)
 
@@ -48,10 +57,10 @@ class SiameseCharCNN(nn.Module):
             x1_cnn_feature = cnn(x1_embedd.permute(0, 2, 1)) # [batch, filters, 1]
             x2_cnn_feature = cnn(x2_embedd.permute(0, 2, 1)) # [batch, filters, 1]
 
-            sub = torch.abs(x1_cnn_feature - x2_cnn_feature).squeeze() # [batch, filters]
+            sub = torch.abs(x1_cnn_feature - x2_cnn_feature).squeeze(-1) # [batch, filters]
             absSub_list.append(sub)
 
-            mul = torch.multiply(x1_cnn_feature, x2_cnn_feature).squeeze() # [batch, filters]
+            mul = torch.multiply(x1_cnn_feature, x2_cnn_feature).squeeze(-1) # [batch, filters]
             mul_list.append(sub)
 
             cos_sim = self.cos_sim_layer(x1_cnn_feature.permute(0, 2, 1), x2_cnn_feature.permute(0, 2, 1)) # [batch, 1]
