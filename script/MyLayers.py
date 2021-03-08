078a55e47c005372f3a7f37bd0291b61c139dc38
wangru <wangru8080@163.com>
2021-03-08 19:59:15 +0800

    Update MyLayers.py

--- script/MyLayers.py
+++ script/MyLayers.py
@@ -9,7 +9,7 @@ import numpy as np
 from collections import OrderedDict
 
 class EuclideanLayer(nn.Module):
-    def __init__(self, dim=1, eps=1e-8, keepdim=False):
+    def __init__(self, dim=1, eps=1e-5, keepdim=False):
         super(EuclideanLayer, self).__init__()
 
         self.dim = dim
@@ -18,3 +18,17 @@ class EuclideanLayer(nn.Module):
 
     def forward(self, x1, x2):
         return torch.sqrt(torch.sum(torch.square(x1 - x2), axis=self.dim, keepdim=self.keepdim) + self.eps)
+
+class CosineLayer(nn.Module):
+    def __init__(self, dim=1, eps=1e-5, keepdim=False):
+        super(CosineLayer, self).__init__()
+
+        self.dim = dim
+        self.eps = eps 
+        self.keepdim = keepdim
+
+    def forward(self, x1, x2):
+        norm1 = torch.sqrt(torch.sum(torch.square(x1), dim=self.dim, keepdim=self.keepdim) + self.eps)
+        norm2 = torch.sqrt(torch.sum(torch.square(x2), dim=self.dim, keepdim=self.keepdim) + self.eps)
+        dot_products = torch.sum(x1 * x2, dim=self.dim, keepdim=self.keepdim)
+        return dot_products / (norm1 * norm2)
