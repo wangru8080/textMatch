183492b02dba2f0946651e66fb5159865801bf06
wangru <wangru8080@163.com>
2021-03-21 17:49:30 +0800

    Update MyLayers.py

--- script/MyLayers.py
+++ script/MyLayers.py
@@ -32,3 +32,36 @@ class CosineLayer(nn.Module):
         norm2 = torch.sqrt(torch.sum(torch.square(x2), dim=self.dim, keepdim=self.keepdim) + self.eps)
         dot_products = torch.sum(x1 * x2, dim=self.dim, keepdim=self.keepdim)
         return dot_products / (norm1 * norm2)
+  
+class SpatialDropout1D(nn.Module):
+    def __init__(self, drop_prob=0.2):
+        super(SpatialDropout1D, self).__init__()
+        self.drop_prob = drop_prob
+    
+    def forward(self, x):
+        output = x.clone()
+        if not self.training or self.drop_prob == 0:
+            return x
+        else:
+            noise = self._make_noise(x)
+            if self.drop_prob == 1:
+                noise.fill_(0)
+            else:
+                noise.bernoulli_(1 - self.drop_prob).div_(1 - self.drop_prob)
+            noise = noise.expand_as(x)
+            output.mul_(noise)
+        return output
+
+    def _make_noise(self, x):
+        return x.new().resize_(x.size(0), *repeat(1, x.dim() - 2), x.size(2))
+
+class SpatialDropout(nn.Module):
+    def __init__(self, drop_prob=0.2):
+        super(SpatialDropout, self).__init__()
+        self.drop_prob = drop_prob
+    
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x = F.dropout2d(x, self.drop_prob, training=self.training)
+        x = x.permute(0, 2, 1)
+        return x
